a706df836765b9c66845da94b82e84754ad1d4cd
Andrea Borelli <andrea.borelli@dgsspa.com>
2024-08-29 18:52:56 +0200

    Inferenza in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,16 @@ import { log } from './main';
 
 const title = 'Typescript PlayGround';
 const author = 'by Fabio Biondi';
+const version = '1.0.0';
 
-log(`${title} ${author}` );
+log(`${title} ${author} ${version}` );
+
+const a: number = 1;
+console.log(a);
+
+
+/* inferenza: inferenz è la possibilità di typescript di generare i tipi al nostro posto  */
+
+let b = 123;
+b = 20; // se scrivamo una stringa al posto di un numero ci da errore perchè b è stato inferito come number
+console.log(b);

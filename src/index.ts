a9d73616fcc1de79fb275389bc1e5953bba347d6
Andrea Borelli <andrea.borelli@dgsspa.com>
2024-08-30 17:12:32 +0200

    Tipizzare oggetti usando interface e gestione proprietà opzionali

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,19 @@ c = true;
 let b = 123; // bisogna evitare di usare any perchè non è una buona pratica
 b = 20; // se scrivamo una stringa al posto di un numero ci da errore perchè b è stato inferito come number
 console.log(b); 
+
+
+// dati in arrivo in modo asincrono, cioè non sappiamo quando arriveranno
+
+interface User {
+    id: number;
+    name: string;
+    username?: string; // ? indica che username è opzionale cioe non è obbligatorio
+}
+
+
+let user: User; // user è un tipo definito in un altro file
+
+user = {id: 123, name: 'Andrea', username: 'bore'}; // initializziamo user con un oggetto 
+user.username = 'lucas'; // non ci da errore perchè user è stato inferito come oggetto
+console.log(user.name, user.username);
